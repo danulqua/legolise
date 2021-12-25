ccd297ebd85c7da35f4addee0a58ac1114326f85
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-12-25 02:29:45 +0200

    Fix heading styles

--- client/src/components/pages/post-page/post-page.jsx
+++ client/src/components/pages/post-page/post-page.jsx
@@ -7,6 +7,7 @@ import Spinner from '../../spinner/spinner.jsx';
 import './post-page.scss';
 import { formatDate } from '../../../helpers';
 import BtnLike from '../../btn-like/btn-like.jsx';
+import { Row, Col } from 'reactstrap';
 
 const PostPage = () => {
   const params = useParams();
@@ -48,21 +49,25 @@ const PostPage = () => {
   console.log(state['0']._id);
   /* add button and user */
   return (
-    <div className='post-box'>
-      <div className='post-page'>
-        <h2>Post Page</h2>
-      </div>
-      <div className='right-side-text'>
-        <li>Title {state['0'].title}</li>
-        <li>Description : {state['0'].description}</li>
-        <li>Created on{formatDate(state['0'].createdOn)}</li>
-      </div>
-      <div className='left-side-pics'>
-        <div className='post-img'>
-          <img src={state['0'].pictures}></img>
-        </div>
-        <li>Number of likes {state['0'].likes.length}</li>
-      </div>
+    <div className='post-wrapper'>
+      <h2>Post Page</h2>
+      <Row>
+        <Col md={4}>
+          <div className='left-side-pics'>
+            <div className='post-img'>
+              <img src={state['0'].pictures}></img>
+            </div>
+            <li>Number of likes {state['0'].likes.length}</li>
+          </div>
+        </Col>
+        <Col md={8}>
+          <div className='right-side-text'>
+            <li>Title {state['0'].title}</li>
+            <li>Description : {state['0'].description}</li>
+            <li>Created on{formatDate(state['0'].createdOn)}</li>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
