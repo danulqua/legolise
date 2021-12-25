eabe274cef84f7d8d24a445854f9b5d12b3a938c
maxim sheyko <maksimsheyko2012@gmail.com>
2021-12-25 17:50:52 +0200

    sauhd

--- client/src/components/post-photos/post-photos.js
+++ client/src/components/post-photos/post-photos.js
@@ -46,6 +46,9 @@ export default class PhotosPost extends Component {
                     </span>
                   </div>
                 )}
+
+
+                
               </div>
             );
           })}
